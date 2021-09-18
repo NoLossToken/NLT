5e326c041035a41af2a41440884c5b8e46b16a8e
No Loss Token <uprikings@gmail.com>
2021-09-18 00:02:24 -0500

    Update nlt.sol

--- nlt.sol
+++ nlt.sol
@@ -1,1 +1,78 @@
+pragma solidity >=0.7.0 <0.9.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract NoLossToken is ERC20 {
+
+ //Main variables
+  address payable public owner;
+  address payable public minter;
+  address payable public commissionAddress;
+  uint public commissionPercentage;
+
+  //Events
+  event TokenMinted(address indexed from, address to);
+  event MinterChanged(address indexed from, address to);
+  event commissionAddressChanged(address indexed from, address to);
+  event commissionPercentageChanged(uint percentage);
+  event tokenMintedToPayUnfreeze(receiverAddress, amount);
+  event ownerMinted(receiverAddress, amount);
+  
+   constructor() payable ERC20() {
+    owner = payable(address(msg.sender));
+    minter = payable(address(msg.sender));
+    commissionAddress = payable(address(msg.sender));
+  }
+  
+  modifier authorizedOwner(){
+    require(msg.sender == owner, "Not Authorized");
+    _;
+  }
+  
+  modifier authorizedToMint(){
+    require(msg.sender == minter || msg.sender == owner, "Not Authorized");
+    _;
+  }
+  
+  //Assign Minter 
+  function passMinterRole(address newMinter) public payable authorizedOwner returns (bool){
+    minter = payable(address(newMinter));
+    emit MinterChanged(msg.sender, newMinter);
+    return true;
+  }
+  
+  //Assign commissionAddress 
+  function passcommissionAddress(address newCommissionAddress) public payable authorizedOwner returns (bool){
+    commissionAddress = payable(address(newCommissionAddress));
+    emit commissionAddressChanged(msg.sender, newCommissionAddress);
+    return true;
+  }
+  
+  //Assign commissionAddress 
+  function changeCommissionPercentage(uint percentage) public payable authorizedOwner returns (bool){
+    commissionPercentage = percentage;
+    emit commissionPercentageChanged(uint percentage);
+    return true;
+  }
+  
+  //Mint For UnFreeze
+  function mintForUnFreeze(address receiverAddress, uint256 amount) public payable authorizedToMint {
+		_mint(receiverAddress, amount);
+    commissionAmount = amount*(commissionPercentage/100)
+    _mint(commissionAddress, commissionAmount);
+    emit tokenMintedToPayUnfreeze(receiverAddress, amount);
+	}
+  
+  //Mint By Owner
+  function mint(address receiverAddress, uint256 amount) public payable authorizedOwner {
+		_mint(receiverAddress, amount);
+    emit ownerMinted(receiverAddress, amount);
+	}
+  
+  function burn(uint256 value) public {        
+    _burn(msg.sender, value);    
+  }    
+  
+  
+} 
+ 
